f6eea3df193f91283e02903b0c4dfd9c49cfb146
Winston Chang <winston@stdout.org>
2018-05-15 12:01:53 -0500

    Compile and hash functions when memoized

--- R/memoize.R
+++ R/memoize.R
@@ -1,8 +1,10 @@
 #' @export
 memoize <- function(f, cache = DiskCache$new()) {
   # f must be forced here to allow code like `g <- memoize(g)` to work.
-  # Otherwise it'll try to recurse infinitely on first invocation.
-  force(f)
+  # Otherwise it'll try to recurse infinitely on first invocation. Also compile
+  # f; otherwise its hash will change on the first two invocations as the
+  # compiler keeps count of how many times it has run, and when it compiles it.
+  f <- compiler::cmpfun(f)
 
   structure(
     function(...) {
@@ -11,7 +13,8 @@ memoize <- function(f, cache = DiskCache$new()) {
       # otherwise, the key could have odd characters that might not work on
       # the filesystem. Hashing is a simple way to guarantee that we'll have
       # a safe filename.
-      args <- list(...)
+      args <- list(f, ...)
+
       key <- digest::digest(args, "sha256")
 
       if (cache$has(key)) {
